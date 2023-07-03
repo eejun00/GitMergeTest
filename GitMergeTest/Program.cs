d7e64e992660a3597a25d13d37834e639f19cbee
DESKTOP-51TGSF5\KGA <dlwnsdud0612@gmail.com>
2023-07-03 14:44:18 +0900

    [ljy] feat: 인치와 센티미터에 관한 클래스,함수 추가

--- GitMergeTest/Program.cs
+++ GitMergeTest/Program.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace GitMergeTest
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }
+    }
+
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54F;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
+        }
+
+        public Ruler(int cmValue) { Centimeter = cmValue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
         }
     }
 }
